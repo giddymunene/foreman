e7d768a18a9bb55f308d2109500449079bb1a115
gideon <gideonmunene0@gmail.com>
2024-08-03 03:52:24 +0300

    app

--- foreman/src/App.js
+++ foreman/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import SiteForm from './components/SiteForm';
+import CostForm from './components/CostForm';
+import CostSummary from './components/CostSummary';
 import './App.css';
 
 function App() {
+  const [site, setSite] = useState('');
+  const [totalCost, setTotalCost] = useState(0);
+  const [currency, setCurrency] = useState('USD');
+  const [costs, setCosts] = useState({
+    materials: 0,
+    transportation: 0,
+    accommodation: 0,
+    labour: 0
+  });
+
+  const handleSiteChange = (name) => {
+    setSite(name);
+  };
+
+  const handleTotalCostChange = (amount) => {
+    setTotalCost(amount);
+  };
+
+  const handleCurrencyChange = (selectedCurrency) => {
+    setCurrency(selectedCurrency);
+  };
+
+  const handleCostsChange = (newCosts) => {
+    setCosts(newCosts);
+  };
+
+  const totalUsed = Object.values(costs).reduce((a, b) => a + b, 0);
+  const warning = totalUsed / totalCost >= 0.6;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Construction Cost Tracker</h1>
+      <SiteForm onSiteChange={handleSiteChange} />
+      <CostForm
+        onTotalCostChange={handleTotalCostChange}
+        onCurrencyChange={handleCurrencyChange}
+        onCostsChange={handleCostsChange}
+      />
+      <CostSummary
+        site={site}
+        totalCost={totalCost}
+        costs={costs}
+        currency={currency}
+        warning={warning}
+      />
     </div>
   );
 }
